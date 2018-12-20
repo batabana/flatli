77c870b3ec66d49d338ec2b2548affc2a814e933
batabana <bata@systemli.org>
2018-12-20 18:33:48 +0100

    littlest changes

--- src/gif.js
+++ src/gif.js
@@ -23,7 +23,7 @@ export default class Gif extends React.Component {
         return (
             <div className="modal" onClick={this.props.hideGif}>
                 <div className="gif modal-inside" onClick={this.notHideGif}>
-                    <img src={this.state.gif} alt="funny cat gif" />
+                    <img src={this.state.gif} alt="cat gif ahead <3" />
                 </div>
             </div>
         );
--- src/header.js
+++ src/header.js
@@ -1,25 +1,13 @@
 import React from "react";
 
-export default class Header extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-
-    render() {
-        return (
-            <header>
-                <img src="acat.png" onClick={this.props.showGif} />
-                <img src="flatli.png" />
-                <a href="/logout">
-                    <img
-                        src={this.props.userimage}
-                        alt={this.props.username}
-                        title={this.props.username}
-                        className="user-icon"
-                    />
-                </a>
-            </header>
-        );
-    }
+export default function Header(props) {
+    return (
+        <header>
+            <img src="acat.png" onClick={props.showGif} />
+            <img src="flatli.png" />
+            <a href="/logout">
+                <img src={props.userimage} alt={props.username} title={props.username} className="user-icon" />
+            </a>
+        </header>
+    );
 }
